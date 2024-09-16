9214becea37d757ccdce290d574babc7557d1893
dakrk <dark@darkok.xyz>
2024-09-16 17:33:04 +0100

    guicommon: set base key in PianoKeyboardWidget

--- src/guicommon/PianoKeyboardWidget.cpp
+++ src/guicommon/PianoKeyboardWidget.cpp
@@ -1,8 +1,11 @@
 #include <QPainter>
 #include "PianoKeyboardWidget.hpp"
 
-PianoKeyboardWidget::PianoKeyboardWidget(QWidget* parent)
-	: QWidget(parent) {}
+PianoKeyboardWidget::PianoKeyboardWidget(QWidget* parent) :
+	QWidget(parent),
+	keyRangeLow_(0),
+	keyRangeHigh_(NUM_KEYS),
+	baseKey_(60) {}
 
 void PianoKeyboardWidget::paintEvent(QPaintEvent* event) {
 	QWidget::paintEvent(event);
@@ -15,18 +18,26 @@ void PianoKeyboardWidget::paintEvent(QPaintEvent* event) {
 	qreal blackKeyW = 0.60 * whiteKeyW;
 	qreal blackKeyH = 0.65 * whiteKeyH;
 
-	for (uint i = 0; i < NUM_WHITE_KEYS; i++) {
-		qreal x = i * whiteKeyW;
-		painter.fillRect(QRectF { x, 0, whiteKeyW, whiteKeyH }, i % 7 ? Qt::white : Qt::gray);
+	for (uint i = 0, white = 0; i < NUM_KEYS; i++) {
+		if (KEY_TABLE[i % 12]) {
+			const auto color = (i == baseKey_) ? BASE_KEY_COLOR : (i % 12 ? WHITE_KEY_COLOR : C_KEY_COLOR);
+			qreal x = white++ * whiteKeyW;
 
-		x += whiteKeyW;
-		painter.setPen(Qt::darkGray);
-		painter.drawLine(x, 0, x, whiteKeyH);
+			painter.fillRect(QRectF { x, 0, whiteKeyW, whiteKeyH }, color);
+			painter.setPen(KEY_BORDER_COLOR);
+			painter.drawLine(QLineF { x + whiteKeyW, 0, x + whiteKeyW, whiteKeyH });
+		}
 	}
 
-	for (uint i = 0; i < NUM_WHITE_KEYS; i++) {
-		if (i % 7 != 2 && i % 7 != 6) {
-			painter.fillRect(QRectF { (i * whiteKeyW) + blackKeyW, 0, blackKeyW, blackKeyH }, Qt::black);
+	for (uint i = 0; i < NUM_KEYS + 1; i++) {
+		if (!KEY_TABLE[i % 12]) {
+			const auto color = (i == baseKey_) ? BASE_KEY_COLOR : BLACK_KEY_COLOR;
+
+			// still not accurate to a real keyboard, but looks perfectly fine at a small scale
+			uint whiteKey = (i / 12) * 7 + KEY_OFFSETS[i % 12];
+			qreal x = ((whiteKey + 1) * whiteKeyW) - (blackKeyW / 2);
+			
+			painter.fillRect(QRectF { x, 0, blackKeyW, blackKeyH }, color);
 		}
 	}
 }
--- src/guicommon/PianoKeyboardWidget.hpp
+++ src/guicommon/PianoKeyboardWidget.hpp
@@ -7,9 +7,45 @@ class GUICOMMON_EXPORT PianoKeyboardWidget : public QWidget {
 public:
 	static constexpr uint NUM_KEYS = 128;
 	static constexpr uint NUM_WHITE_KEYS = 75;
+	static constexpr bool KEY_TABLE[] = { 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1 };
+	static constexpr uint KEY_OFFSETS[] = { 0, 0, 1, 1, 2, 3, 3, 4, 4, 5, 5, 6 };
+
+	static constexpr auto WHITE_KEY_COLOR = Qt::white;
+	static constexpr auto BLACK_KEY_COLOR = Qt::black;
+	static constexpr auto BASE_KEY_COLOR = Qt::red;
+	static constexpr auto C_KEY_COLOR = Qt::gray;
+	static constexpr auto KEY_BORDER_COLOR = Qt::darkGray;
 
 	explicit PianoKeyboardWidget(QWidget* parent = nullptr);
 
+	QPair<uint, uint> keyRange() const {
+		return { keyRangeLow_, keyRangeHigh_ };
+	}
+
+	void setKeyRange(uint low, uint high) {
+		keyRangeLow_ = low;
+		keyRangeHigh_ = high;
+		repaint();
+	}
+
+	void setKeyRange(QPair<uint, uint> range) {
+		setKeyRange(range.first, range.second);
+	}
+
+	uint baseKey() const {
+		return baseKey_;
+	}
+
+	void setBaseKey(uint key) {
+		baseKey_ = key;
+		repaint();
+	}
+
 protected:
 	void paintEvent(QPaintEvent* event) override;
+
+private:
+	uint keyRangeLow_;
+	uint keyRangeHigh_;
+	uint baseKey_;
 };
